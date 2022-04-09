1a43a93b59e4dde64229bdeaff21399d14d24bc3
tarasfl <tfliorko@gmail.com>
2022-04-09 22:51:12 +0300

    finaly lab3

--- src/main/java/museum/Painting.java
+++ src/main/java/museum/Painting.java
@@ -2,9 +2,9 @@ package museum;
 
 import java.util.Date;
 
-class Painting extends Exhibit{
-    private float height;
-    private float length;
+public class Painting extends Exhibit{
+    private double height;
+    private double length;
     private String style;
 
     public Painting(){
@@ -13,7 +13,7 @@ class Painting extends Exhibit{
         length=0;
         style="modno";
     }
-    public Painting(float obj_height, float obj_length, String obj_style, String author, Date year, String origin, String theme, Date date_of_presenting){
+    public Painting(double obj_height, double obj_length, String obj_style, String author, Date year, String origin, String theme, Date date_of_presenting){
         super(author, year, origin, theme, date_of_presenting);
         height=obj_height;
         length=obj_length;
